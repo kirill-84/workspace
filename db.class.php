0684505cf8368567206f771e690c00fabbec86ab
Kirill <35264591+kirill-84@users.noreply.github.com>
2018-01-10 16:54:30 +0300

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -1,4 +1,13 @@
 <?php
-
-
+class DB {
+  protected $connection;
+  
+  public function __construct($host, $user, $password, $db_name) {
+    $this->connection = new mysqli($host, $user, $password, $db_name);
+    
+    if( !$this->connection ) {
+      throw new Exception('Could not connect to DB');
+    }
+  }
+}
 ?>
