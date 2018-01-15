29f0aef3dc0fe5b2c495919c3c82cba0670bbb96
Kirill <35264591+kirill-84@users.noreply.github.com>
2018-01-15 22:30:42 +0300

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -11,5 +11,34 @@ class DB {
       throw new Exception('Could not connect to DB');
     }
   }
+  public function query($sql) {
+    if ( !$this->connection ) {
+      return false;
+    }
+    
+    $result = $this->connection->query($sql);
+    
+    if(mysqli_error($this->connection)){
+      throw new Exception(mysqli_error($this->connection));
+    }
+    
+    if(is_bool($result)){
+      return $result;
+    }
+    
+    $data = array();
+    
+    while($row = mysqli_fetch_assoc($result)){
+      $data[] = $row;
+    }
+    
+    mysqli_free_result($sql);
+    
+    return $data;
+  }
+  
+  public function escape($str){
+    return mysqli_escape_string($this->connection, $str);
+  }
 }
 ?>
