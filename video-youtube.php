16e6346e4fb632d691e2d54c242d47df2ede902a
Kirill <35264591+kirill-84@users.noreply.github.com>
2021-11-12 22:11:04 +0300

    Create file video-youtube.php
    Added last four video from youtube channel

---
+++ video-youtube.php
@@ -0,0 +1,29 @@
+<?php
+// Added last four video from youtube channel
+$API_Key    = 'AIzaSyDT31pKDmCRbWdrOLywJlun9Kahoja0JNQ'; 
+$Channel_ID = 'UCUs9v1KvcSfZIc34PTviqOQ'; 
+$Max_Results = 4;
+
+$apiError = 'Video not found';
+try{
+	$apiData = @file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$Channel_ID.'&maxResults='.$Max_Results.'&key='.$API_Key.'');
+	if($apiData){
+    $videoList = json_decode($apiData); 
+	}else{ 
+	    throw new Exception('Invalid API key or channel ID.'); 
+	}
+}catch(Exception $e){
+	$apiError = $e->getMessage();
+}
+
+if(!empty($videoList->items)){
+	foreach($videoList->items as $item){
+		if(isset($item->id->videoId)){?>
+			<div><img src="<?= $item->snippet->thumbnails->medium->url; ?>"/></div>
+			<div><?= $item->snippet->title; ?></div>
+		<?}
+	}
+} else {
+	echo '<p>'.$apiError.'</p>';
+}
+?>
