35c07ef7366f6daff45be6cc4848a4d6b32bb8eb
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 09:42:02 +0300

    Markings have been added

--- video-youtube.php
+++ video-youtube.php
@@ -5,23 +5,23 @@ $Channel_ID = '';
 $Max_Results = 4;
 
 $apiError = 'Video not found';
-try{
+try {
 	$apiData = @file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$Channel_ID.'&maxResults='.$Max_Results.'&key='.$API_Key.'');
-	if($apiData){
+	if($apiData) {
     		$videoList = json_decode($apiData); 
-	}else{
-	    throw new Exception('Invalid API key or channel ID.'); 
+	} else {
+		throw new Exception('Invalid API key or channel ID.'); 
 	}
-}catch(Exception $e){
+} catch(Exception $e) {
 	$apiError = $e->getMessage();
 }
 
-if(!empty($videoList->items)){
-	foreach($videoList->items as $item){
-		if(isset($item->id->videoId)){?>
+if(!empty($videoList->items)) {
+	foreach($videoList->items as $item) {
+		if(isset($item->id->videoId)) {?>
 			<div><img src="<?= $item->snippet->thumbnails->medium->url; ?>"/></div>
 			<div><?= $item->snippet->title; ?></div>
-		<?}
+		<? }
 	}
 } else {
 	echo '<p>'.$apiError.'</p>';
